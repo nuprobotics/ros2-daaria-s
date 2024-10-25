c4dc3600598de61cfcead4805bc2e8448ef9bf11
Daria.Suvorova <daria.suvorova@jetbrains.com>
2024-10-25 11:45:04 +0300

    task03

---
+++ Practice02/src/task03/task03/service.py
@@ -0,0 +1,42 @@
+import rclpy
+from rclpy.node import Node
+from std_srvs.srv import Trigger
+
+class MyService(Node):
+    def __init__(self):
+        super(MyService, self).__init__("my_service")
+
+        self.declare_parameter('service_name', '/trigger_service')
+        self.declare_parameter('default_string', 'No service available')
+
+        self.service_name = self.get_parameter("service_name").get_parameter_value().string_value
+        self.default_string = self.get_parameter("default_string").get_parameter_value().string_value
+
+
+        self.client = self.create_client(Trigger, "/spgc/trigger")
+
+        self.string_to_store = ""
+
+        self.service = self.create_service(Trigger, self.service_name, self.service_cb)
+
+        if not self.client.wait_for_service(timeout_sec=1.0):
+            self.string_to_store = self.default_string
+        else:
+            future = self.client.call_async(Trigger.Request())
+            rclpy.spin_until_future_complete(self, future)
+            self.string_to_store = future.result().message
+
+    def service_cb(self, request, response):
+        response.success = True
+        response.message = self.string_to_store
+        return response
+
+def main(args=None):
+    rclpy.init(args=args)
+    my_service = MyService()
+    rclpy.spin(my_service)
+    my_service.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
